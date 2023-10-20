4f2cc8b786f9c47b30ed6d5f2e9804c89328975a
Robin-Lippert <20101365@tafe.wa.edu.au>
2023-10-20 12:06:02 +0800

    added notation function

--- Assets/Scripts/EggsCounter.cs
+++ Assets/Scripts/EggsCounter.cs
@@ -7,10 +7,12 @@ using System;
 
 public class EggsCounter : MonoBehaviour
 {
-    public float currentTotalEggs = 0f;
-    public float clickIncrement = 1f;
-    public float EPS = 0f;
+    public double currentTotalEggs = 0f;
+    public double clickIncrement = 1f;
+    public double EPS = 0f;
     public TMP_Text eggsText;
+    string strCurrentTotalEggs;
+    double[] bigNumberList = {Math.Pow(10,3), Math.Pow(10, 6), Math.Pow(10, 9), Math.Pow(10, 12), Math.Pow(10, 15), Math.Pow(10, 18) };
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +21,77 @@ public class EggsCounter : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-    
-        eggsText.text = $"{Math.Floor(currentTotalEggs)} Eggs";
+
+        currentTotalEggs += EPS * Time.fixedDeltaTime;
+        eggsText.text = $"{formatNumber(currentTotalEggs)} Eggs";
+       
+        /*
+        if(currentTotalEggs < bigNumberList[0])
+        {
+            strCurrentTotalEggs = Math.Floor(currentTotalEggs).ToString();
+        }
+        else if (currentTotalEggs >= bigNumberList[0] &&  currentTotalEggs < bigNumberList[1]) 
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[0], 2).ToString() + "k";
+        }
+        else if (currentTotalEggs >= bigNumberList[1] && currentTotalEggs < bigNumberList[2])
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[1], 2).ToString() + "m";
+        }
+        else if (currentTotalEggs >= bigNumberList[2] && currentTotalEggs < bigNumberList[3])
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[2], 2).ToString() + "b";
+        }
+        else if (currentTotalEggs >= bigNumberList[3] && currentTotalEggs < bigNumberList[4])
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[3], 2).ToString() + "t";
+        }
+        else if (currentTotalEggs >= bigNumberList[4] && currentTotalEggs < bigNumberList[5])
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[4], 2).ToString() + "aa";
+        }
+        else if (currentTotalEggs >= bigNumberList[5])
+        {
+            strCurrentTotalEggs = RoundDown(currentTotalEggs / bigNumberList[5], 2).ToString() + "ab";
+        }
+
+        eggsText.text = $"{strCurrentTotalEggs} Eggs";
+        */
+
+
+    }
+
+    string formatNumber(double number)
+    {
+        string[] bigNumberNotations = { "k", "m", "b", "t", "aa", "ab", "ac", "ad", "ae", "af", "ag", "ah", "ai", "aj", "ak", "al", "am", "an", "ao", "ap", "aq", "ar", "as", "at", "au", "av", "aw", "ax","ay", "az" };
+        bool highNumber = false;
+        double bigNumber = 1000D;
+        int tabPosition = -1;
+        string notation;
+        if (number >= bigNumber)
+        {
+            highNumber = true;
+            while(number >= bigNumber) { bigNumber *= 1000; tabPosition++; }
+            number /= (bigNumber /1000);
+            notation = bigNumberNotations[tabPosition];
+            return RoundDown(number, 2) + notation;
+        }
+        else 
+        {
+            notation = "";
+            return RoundDown(number, 0) + notation;
+        }
+        
+        
+
+        
+    }
+
+
+    public double RoundDown(double number, int decimalPlaces)
+    {
+        return Math.Floor(number * Math.Pow(10, decimalPlaces)) / Math.Pow(10, decimalPlaces);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IdleBaldClicker
+
+bignum func help
+https://forum.unity.com/threads/creating-and-formatting-huge-numbers.400790/
